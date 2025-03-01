a91723a92f87533ce26bdb58db9f1c4765774da2
Suonbicha11052005 <nguyenthihoangvi.1126@gmail.com>
2025-03-01 21:46:17 +0700

    Update ex6.sql

--- Practice2/ex6.sql
+++ Practice2/ex6.sql
@@ -3,7 +3,7 @@ the year and last post of the year in the year 2021.
 Chi xet nhung user co so bai post trong nam 2021 it nhat la 2.
 output the user and number of the days between*/
 SELECT user_id,
-MAX(DATE(post_date))-MIN(DATE(post_date)) AS days_between
+MAX(post_date)-MIN(post_date) AS days_between
 FROM posts
 WHERE post_date BETWEEN '2021-01-01' AND '2022-01-01'
 GROUP BY user_id
