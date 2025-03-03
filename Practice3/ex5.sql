c79462735a066518ed130769f77b7ea7add68f46
Suonbicha11052005 <nguyenthihoangvi.1126@gmail.com>
2025-03-03 22:55:23 +0700

    Update ex5.sql

--- Practice3/ex5.sql
+++ Practice3/ex5.sql
@@ -7,6 +7,7 @@ SELECT sender_id,
 COUNT(message_id) AS message_count
 FROM messages
 WHERE sent_date BETWEEN '2022-08-01' AND '2022-09-01'
+--WHERE EXTRACT(month FROM sent_date)=8 AND EXTRACT(year FROM sent_date) = 2022
 GROUP BY sender_id
 ORDER BY message_count DESC
 LIMIT 2;
