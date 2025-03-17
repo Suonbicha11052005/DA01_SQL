dee715424e40ceb71b578ac3da1d4e0bf00e5550
Suonbicha11052005 <nguyenthihoangvi.1126@gmail.com>
2025-03-18 00:45:10 +0700

    Create ex9.sql

---
+++ Practice6/ex9.sql
@@ -0,0 +1,8 @@
+/*Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
+
+Return the result table ordered by employee_id.*/
+SELECT employee_id
+FROM Employees
+WHERE salary<30000 AND manager_id NOT IN(SELECT employee_id
+FROM Employees) AND manager_id IS NOT NULL
+ORDER BY employee_id
